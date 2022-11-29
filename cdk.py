14ec67611b8c247af959b915384eab41f0c9588c
Andre Nobre <andrenobre@Andres-MacBook-Pro.local>
2022-11-29 23:16:36 +0000

    Adding custom boostrap template

--- cdk.py
+++ cdk.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+# !/usr/bin/env python3
 
 import os
 import aws_cdk as cdk
@@ -14,13 +14,18 @@ from constructs import Construct
 
 
 class Infra(Stack):
-    def __init__(self, scope: Construct, construct_id: str, repo: str,
-                 artifact_bucket: str, lambda_key_a: str, **kwargs) -> None:
+    def __init__(self, 
+                 scope: Construct, 
+                 construct_id: str, 
+                 repo: str,
+                 artifact_bucket: str, 
+                 lambda_key_a: str, 
+                 **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
         # IAM Role
         lambda_role = iam_.Role(scope=self,
-                                id=f"{repo}-lambda-role",
+                                id='lambda-role',
                                 assumed_by=iam_.ServicePrincipal('lambda.amazonaws.com'),
                                 role_name=f"{repo}-lambda-role",
                                 managed_policies=[
@@ -29,47 +34,33 @@ class Infra(Stack):
                                 ])
         # Lambda code artifact bucket
         artifact_s3 = s3.Bucket.from_bucket_attributes(
-            self, 'lambda-artifact-bucket',
+            self, 'artifact-bucket',
             bucket_name=artifact_bucket
         )
 
         # Lambda Function
         lambda_a = lambda_.Function(
             scope=self,
-            id=f"{repo}-lambda-a",
+            id='lambda-a',
             code=lambda_.S3Code(
                 bucket=artifact_s3,
                 key=lambda_key_a),
             handler="app.lambda_handler",
             runtime=lambda_.Runtime.PYTHON_3_9,
-            architecture=None,
-            current_version_options=None,
-            description=None,
-            environment=None,
             function_name=f"{repo}-etl",
-            initial_policy=None,
-            insights_version=None,
-            layers=None,
-            log_retention=None,
-            log_retention_retry_options=None,
-            log_retention_role=None,
-            memory_size=None,
-            reserved_concurrent_executions=None,
             role=lambda_role,
             timeout=Duration.seconds(10)
         )
-        # Adding Tags to Lambda
-        Tags.of(lambda_a).add("Repository", repo)
 
         # CDK outputs
-        cdk.CfnOutput(scope=self, id='cdk-lambda-a-output',
+        cdk.CfnOutput(scope=self, id='lambda-a-name',
                       value=lambda_a.function_name)
 
 
 app = cdk.App()
 
 Infra(app,
-      'andrenobre-cdk',
+      'api-convert-json-2-csv',
       env=cdk.Environment(
           account=os.getenv('CDK_DEFAULT_ACCOUNT'),
           region=os.getenv('CDK_DEFAULT_REGION'),
---
+++ dev.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+# CDK env:
+export CDK_DEFAULT_ACCOUNT="457973886815"
+export CDK_DEFAULT_REGION="eu-west-2"
+
+# AWS Stack variables:
+export REPO_NAME="api-convert-json-2-csv"
+export ENVIRONMENT="dev"
+export GIT_COMMIT_ID="c2179d7"
+export ARTIFACT_BUCKET="core-infra-deployment-artifacts"
+export LAMBDA_KEY_A="api-convert-json-2-csv/lambda-a.zip"
